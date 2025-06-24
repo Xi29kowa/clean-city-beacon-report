ee142f4126d1640047d27ff7c26ff70c6766f4ce
Christian Müller <130841585+Xi29kowa@users.noreply.github.com>
2025-06-24 12:06:08 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -12,7 +11,6 @@ import Impressum from "./pages/Impressum";
 import Nutzungsbedingungen from "./pages/Nutzungsbedingungen";
 import Account from "./pages/Account";
 import Karte from './pages/Karte'; // oder './pages/Karte' – je nachdem wo du sie gespeichert hast
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 const queryClient = new QueryClient();
@@ -24,15 +22,15 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/account" element={<Account />} />
-            <Route path="/datenschutz" element={<Datenschutz />} />
-            <Route path="/impressum" element={<Impressum />} />
-            <Route path="/nutzungsbedingungen" element={<Nutzungsbedingungen />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+         <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/datenschutz" element={<Datenschutz />} />
+          <Route path="/impressum" element={<Impressum />} />
+          <Route path="/nutzungsbedingungen" element={<Nutzungsbedingungen />} />
+          <Route path="/karte" element={<Karte />} />
+          <Route path="*" element={<NotFound />} />
+         </Routes>
         </BrowserRouter>
       </TooltipProvider>
     </AuthProvider>
